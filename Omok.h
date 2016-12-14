f83718d3994003fca1df3317c40c05a4be4215e0
ParkDJ <djpark@knu.ac.kr>
2016-12-14 23:57:47 +0900

    Omok.c server.c Omok.h
    Signed-off-by: ParkDJ <djpark@knu.ac.kr>

---
+++ Omok.h
@@ -0,0 +1,14 @@
+int setSocket(); 
+void print(int, int, char);
+int isVictory(char);
+int put(int,int,char);
+int six(int,int,char);
+int threethree(int, int, char);
+void print_stat();
+void set_up();
+void start();
+void resetMap();
+void clearbottom();
+void printbottom(int);
+
+char map[15][29] = { 0, };
