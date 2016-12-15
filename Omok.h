7ef34005215ff18cc8dabf7820337acb71c4bf85
921120 <djpark@knu.ac.kr>
2016-12-16 03:11:02 +0900

    Final

--- Omok.h
+++ Omok.h
@@ -1,14 +1,14 @@
 int setSocket(); 
 void print(int, int, char);
-int isVictory(char);
 int isVictory3(char);
 int isVictory4(char);
-int isDraw();
+int isVictory5(char);
 int isDraw3();
 int isDraw4();
-int put(int,int,char);
+int isDraw5();
 int put3(int,int,char);
 int put4(int,int,char);
+int put5(int,int,char);
 int six(int,int,char);
 int threethree(int, int, char);
 void print_stat();
@@ -22,4 +22,4 @@ char map3[3][5] = { 0, };
 
 char map4[8][17] = { 0, };
 
-char map[15][29] = {0, };
+char map5[15][29] = {0, };
