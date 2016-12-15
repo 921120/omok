a9476b3885ca2acf46f2fbde1754daaa35222b03
921120 <djpark@knu.ac.kr>
2016-12-16 02:04:32 +0900

    4mok만든 부분 + 4목 헤더파일

---
+++ 4Omok.h
@@ -0,0 +1,14 @@
+int setSocket(); 
+void print(int, int, char);
+int isVictory(char);
+int put4(int,int,char);
+int six(int,int,char);
+int threethree(int, int, char);
+void print_stat();
+void set_up();
+void start();
+void resetMap();
+void clearbottom();
+void printbottom(int);
+
+char map4[8][17] = { 0, };
